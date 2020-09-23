62ac0df71b075800f3b8e623e33b012fafdae725
Denys Kublytskyi <deniskublitskiy@gmail.com>
2020-09-23 23:41:35 +0300

    :sparkles: Add loader instead of "Loading..." text

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from "react";
 import { MuiPickersUtilsProvider } from "@material-ui/pickers";
 import DateFnsUtils from "@date-io/date-fns";
 
+import Box from "@material-ui/core/Box";
+import CircularProgress from "@material-ui/core/CircularProgress";
+
 import MainApp from "./MainApp";
 import Auth from "./Auth";
 import { AppContextProvider, useAppContext } from "./context/appContext";
@@ -11,7 +14,16 @@ const App = () => {
     const { isLoading, user } = useAppContext();
 
     if (isLoading) {
-        return <div>Loading...</div>;
+        return (
+            <Box
+                display="flex"
+                alignItems="center"
+                justifyContent="center"
+                height="100vh"
+            >
+                <CircularProgress />
+            </Box>
+        );
     }
 
     return user ? <MainApp /> : <Auth />;
